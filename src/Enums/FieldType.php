da1af31039ccf63f91fc7afa3817533a7f263c16
rillyv <rillyv.dev@gmail.com>
2024-02-21 14:36:13 +0100

    feat: changed $fieldValue to $field and throw exception

--- src/Enums/FieldType.php
+++ src/Enums/FieldType.php
@@ -2,67 +2,70 @@
 
 namespace CraftFieldConnector\Enums;
 
+
 enum FieldType: string
 {
-    case Assets = 'Assets';
-    case Categories = 'Categories';
-    case Checkboxes = 'Checkboxes';
-    case Color = 'Color';
-    case Country = 'Country';
-    case Date = 'Date';
-    case Dropdown = 'Dropdown';
-    case Email = 'Email';
-    case Embed = 'Embed';
-    case Entries = 'Entries';
-    case LightSwitch = 'LightSwitch';
-    case Matrix = 'Matrix';
-    case Map = 'Map';
-    case MissingField = 'MissingField';
-    case Money = 'Money';
-    case MultiSelect = 'MultiSelect';
-    case Number = 'Number';
-    case PlainText = 'PlainText';
-    case Position = 'Position';
-    case RadioButtons = 'RadioButtons';
-    case Redactor = 'Redactor';
-    case SuperTable = 'SuperTable';
-    case Table = 'Table';
-    case Tags = 'Tags';
-    case Time = 'Time';
-    case Url = 'Url';
-    case Users = 'Users';
-    case Unknown = 'Unknown';
+    case Assets = 'assets';
+    case Categories = 'categories';
+    case Checkboxes = 'checkboxes';
+    case Color = 'color';
+    case Country = 'country';
+    case Date = 'date';
+    case Dropdown = 'dropdown';
+    case Email = 'email';
+    case Embed = 'embed';
+    case Entries = 'entries';
+    case LightSwitch = 'lightSwitch';
+    case Matrix = 'matrix';
+    case Map = 'map';
+    case MissingField = 'missingField';
+    case Money = 'money';
+    case MultiSelect = 'multiSelect';
+    case Number = 'number';
+    case PlainText = 'plainText';
+    case Position = 'position';
+    case RadioButtons = 'radioButtons';
+    case Redactor = 'redactor';
+    case SuperTable = 'superTable';
+    case Table = 'table';
+    case Tags = 'tags';
+    case Time = 'time';
+    case Url = 'url';
+    case Users = 'users';
 
-    public static function matchFromCraftField($fieldValue): ?self
+    /**
+     * @throws \Exception
+     */
+    public static function matchFromCraftField($field): ?self
     {
         return match (true) {
-            $fieldValue instanceof \craft\fields\Assets => FieldType::Assets,
-            $fieldValue instanceof \craft\fields\Categories => FieldType::Categories,
-            $fieldValue instanceof \craft\fields\Checkboxes => FieldType::Checkboxes,
-            $fieldValue instanceof \craft\fields\data\ColorData => FieldType::Color,
-            $fieldValue instanceof \craft\fields\Country => FieldType::Country,
-            $fieldValue instanceof \craft\fields\Date => FieldType::Date,
-            $fieldValue instanceof \craft\fields\Dropdown => FieldType::Dropdown,
-            $fieldValue instanceof \craft\fields\Email => FieldType::Email,
-            $fieldValue instanceof \modules\embedsmodule\fields\Embed => FieldType::Embed,
-            $fieldValue instanceof \craft\fields\Entries => FieldType::Entries,
-            $fieldValue instanceof \craft\fields\LightSwitch => FieldType::LightSwitch,
-            $fieldValue instanceof \craft\fields\Matrix => FieldType::Matrix,
-            $fieldValue instanceof \ether\simplemap\fields\MapField => FieldType::Map,
-            $fieldValue instanceof \craft\fields\Money => FieldType::Money,
-            $fieldValue instanceof \craft\fields\MultiSelect => FieldType::MultiSelect,
-            $fieldValue instanceof \craft\fields\Number => FieldType::Number,
-            $fieldValue instanceof \craft\fields\PlainText => FieldType::PlainText,
-            $fieldValue instanceof \rias\positionfieldtype\fields\Position => FieldType::Position,
-            $fieldValue instanceof \craft\fields\RadioButtons => FieldType::RadioButtons,
-            $fieldValue instanceof \craft\redactor\Field => FieldType::Redactor,
-            $fieldValue instanceof \verbb\supertable\fields\SuperTableField => FieldType::SuperTable,
-            $fieldValue instanceof \craft\fields\Table => FieldType::Table,
-            $fieldValue instanceof \craft\fields\Tags => FieldType::Tags,
-            $fieldValue instanceof \craft\fields\Time => FieldType::Time,
-            $fieldValue instanceof \craft\fields\Url => FieldType::Url,
-            $fieldValue instanceof \craft\fields\Users => FieldType::Users,
-            default => FieldType::Unknown,
+            $field instanceof \craft\fields\Assets => FieldType::Assets,
+            $field instanceof \craft\fields\Categories => FieldType::Categories,
+            $field instanceof \craft\fields\Checkboxes => FieldType::Checkboxes,
+            $field instanceof \craft\fields\Color => FieldType::Color,
+            $field instanceof \craft\fields\Country => FieldType::Country,
+            $field instanceof \craft\fields\Date => FieldType::Date,
+            $field instanceof \craft\fields\Dropdown => FieldType::Dropdown,
+            $field instanceof \craft\fields\Email => FieldType::Email,
+            $field instanceof \modules\embedsmodule\fields\Embed => FieldType::Embed,
+            $field instanceof \craft\fields\Entries => FieldType::Entries,
+            $field instanceof \craft\fields\LightSwitch => FieldType::LightSwitch,
+            $field instanceof \craft\fields\Matrix => FieldType::Matrix,
+            $field instanceof \ether\simplemap\fields\MapField => FieldType::Map,
+            $field instanceof \craft\fields\Money => FieldType::Money,
+            $field instanceof \craft\fields\MultiSelect => FieldType::MultiSelect,
+            $field instanceof \craft\fields\Number => FieldType::Number,
+            $field instanceof \craft\fields\PlainText => FieldType::PlainText,
+            $field instanceof \rias\positionfieldtype\fields\Position => FieldType::Position,
+            $field instanceof \craft\fields\RadioButtons => FieldType::RadioButtons,
+            $field instanceof \craft\redactor\Field => FieldType::Redactor,
+            $field instanceof \verbb\supertable\fields\SuperTableField => FieldType::SuperTable,
+            $field instanceof \craft\fields\Table => FieldType::Table,
+            $field instanceof \craft\fields\Tags => FieldType::Tags,
+            $field instanceof \craft\fields\Time => FieldType::Time,
+            $field instanceof \craft\fields\Url => FieldType::Url,
+            $field instanceof \craft\fields\Users => FieldType::Users,
+            default => throw new \Exception("No enum found for $field"),
         };
     }
 
